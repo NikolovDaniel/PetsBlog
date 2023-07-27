31eef431f5ece968c293cd28039dab212613534b
Daniel Nikolov <danielnikolov@Daniels-MacBook-Air.local>
2023-07-27 15:54:00 +0300

    changed petedit redirect button

--- PetBlog (FE)/src/Components/Pet/PetEdit.jsx
+++ PetBlog (FE)/src/Components/Pet/PetEdit.jsx
@@ -223,7 +223,6 @@ const PetEdit = () => {
                         <p className='fs-5'>You have successfully edited your Pet.</p>
                         <Link to={`/pet/${petIdField}`} className='custom-button btn fs-5 border-0'>View your Pet</Link>
                     </div>
-
                 </React.Fragment>} />
             <ErrorModal show={showErrorModal} onClose={handleCloseErrorModal} errorMessages={errorMessages} />
         </Container >
