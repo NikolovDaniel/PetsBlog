e4623642ca183cceee468a5ddc18e15b70bcf352
Daniel Nikolov <danielnikolov@Daniels-MacBook-Air.local>
2023-07-28 09:44:54 +0300

    fixed bugs

--- PetBlog (FE)/src/Components/Image/Images.jsx
+++ PetBlog (FE)/src/Components/Image/Images.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col, Card, Form } from 'react-bootstrap';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Images from '../Image/Images'
 import "../CSS/PetById.css";
 
 const Images = ({ id }) => {
