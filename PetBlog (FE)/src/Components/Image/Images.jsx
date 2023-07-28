71c898b5b15b70b8bb2b80586d28e77db0f3c7f2
Daniel Nikolov <danielnikolov@Daniels-MacBook-Air.local>
2023-07-28 09:35:52 +0300

    more changes in images component

--- PetBlog (FE)/src/Components/Image/Images.jsx
+++ PetBlog (FE)/src/Components/Image/Images.jsx
@@ -4,15 +4,15 @@ import "../CSS/PetById.css";
 
 const Images = ({ id }) => {
     const [selectedCategory, setSelectedCategory] = useState('All');
+    const [images, setImages] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [imagesLoaded, setImagesLoaded] = useState(false);
 
     const fetchImages = () => {
         setLoading(true);
         axios
             .get(`https://kolombus-001-site1.htempurl.com/api/Images/${id}`)
             .then((response) => {
-                setPet(response.data);
+                setImages(response.data);
                 setLoading(false);
             })
             .catch((error) => {
@@ -21,38 +21,17 @@ const Images = ({ id }) => {
             });
     };
 
-    const fetchPetImages = () => {
-        const imagePromises = pet.images.map((image) =>
-            new Promise((resolve, reject) => {
-                const img = new Image();
-                img.src = `data:image/jpeg;base64,${image.image}`;
-                img.onload = resolve;
-                img.onerror = reject;
-            })
-        );
-
-        Promise.allSettled(imagePromises)
-            .then(() => setImagesLoaded(true))
-            .catch(() => setImagesLoaded(true));
-    };
-
     useEffect(() => {
         fetchImages();
     }, []);
-
-    useEffect(() => {
-        if (!loading && Object.keys(pet).length > 0) {
-            fetchPetImages();
-        }
-    }, [pet, loading]);
 
     if (loading) {
         return <div className='headers text-center fs-3'>Loading...</div>;
     }
 
-    const allCategories = ['All', ...new Set(Object.values(pet.images).map((image) => image.category))];
+    const allCategories = ['All', ...new Set(Object.values(images).map((image) => image.category))];
 
-    const filteredImages = selectedCategory === 'All' ? pet.images : pet.images.filter((image) => image.category === selectedCategory);
+    const filteredImages = selectedCategory === 'All' ? images : images.filter((image) => image.category === selectedCategory);
 
     return (
         <>
@@ -79,19 +58,17 @@ const Images = ({ id }) => {
                     </Form>
                 </Col>
             </Row>
-            {imagesLoaded && (
-                <Row className='gallery d-flex justify-content-center mt-5'>
-                    {images ? images.map((img, index) => (
-                        <Col className='pt-2 pb-2' key={index} xs={6} md={4} lg={3}>
-                            <div className='pet-picture-wrapper'>
-                                <Card>
-                                    <Card.Img className='pet-picture' loading='lazy' variant="top" src={`data:image/jpeg;base64,${img.image}`} alt={`Dog ${index + 1}`} />
-                                </Card>
-                            </div>
-                        </Col>
-                    )) : <div className='text-center fs-3'>No images available!</div>}
-                </Row>
-            )}
+            <Row className='gallery d-flex justify-content-center mt-5'>
+                {filteredImages ? filteredImages.map((img, index) => (
+                    <Col className='pt-2 pb-2' key={index} xs={6} md={4} lg={3}>
+                        <div className='pet-picture-wrapper'>
+                            <Card>
+                                <Card.Img className='pet-picture' loading='lazy' variant="top" src={`data:image/jpeg;base64,${img.image}`} alt={`Dog ${index + 1}`} />
+                            </Card>
+                        </div>
+                    </Col>
+                )) : <div className='text-center fs-3'>No images available!</div>}
+            </Row>
         </>
     );
 }
